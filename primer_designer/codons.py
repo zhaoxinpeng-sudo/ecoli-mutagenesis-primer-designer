4fe7025431b218eb948e12cab7fe3a7d0068f6ab
zhaoxinpeng-sudo <13945708594@163.com>
2026-08-20 00:59:33 +0800

    Initial release: add primer_designer/codons.py

---
+++ primer_designer/codons.py
@@ -0,0 +1,14 @@
+"""Preferred E. coli K-12 codons, ordered approximately by cellular usage."""
+
+ECOLI_CODONS = {
+    "A": ["GCG", "GCC", "GCT", "GCA"], "C": ["TGC", "TGT"], "D": ["GAT", "GAC"],
+    "E": ["GAA", "GAG"], "F": ["TTT", "TTC"], "G": ["GGC", "GGT", "GGG", "GGA"],
+    "H": ["CAT", "CAC"], "I": ["ATT", "ATC", "ATA"], "K": ["AAA", "AAG"],
+    "L": ["CTG", "TTA", "TTG", "CTT", "CTC", "CTA"], "M": ["ATG"],
+    "N": ["AAC", "AAT"], "P": ["CCG", "CCA", "CCT", "CCC"], "Q": ["CAG", "CAA"],
+    "R": ["CGT", "CGC", "CGG", "CGA", "AGA", "AGG"], "S": ["AGC", "TCT", "TCC", "TCG", "AGT", "TCA"],
+    "T": ["ACC", "ACG", "ACT", "ACA"], "V": ["GTG", "GTT", "GTC", "GTA"],
+    "W": ["TGG"], "Y": ["TAT", "TAC"],
+}
+
+
